54e643b3ae03e61d3dccc670e989758e1cd88028
AhmedNadar <ahmedfay@hotmail.com>
2016-03-18 14:02:27 -0400

    Create user profile form

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,6 +1,25 @@
 class ProfilesController < ApplicationController
   def new
-    @user = User.find(params[:user_id])
-    @profiles = @user.build_profile
+    @user    = User.find(params[:user_id])
+    @profile = @user.build_profile
   end
+
+  def create
+    @user    = User.find(params[:user_id])
+    @profile = @user.build_profile(profile_params)
+
+    if @profile.save
+      flash[:success] = "Your profile is been created."
+      redirect_to user_path(params[:user_id])
+    else
+      flash[:ganger] = "You are missing some info, please try again..."
+      render action: :new
+    end
+  end
+
+
+  private
+    def profile_params
+      params.require(:profile).permit(:first_name, :last_name, :job_title, :description, :phone_number, :location )
+    end
 end
